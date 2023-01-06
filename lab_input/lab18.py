aa5461fbebd34d8b468f5a9450d17e1c0e7b9bd0
sueanyv <valenciasue@hotmail.com>
2023-01-06 21:58:34 +0000

    code

---
+++ lab_input/lab18.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+     name = input("What is your name?")
+     day = input("What day is today?")
+     print("Hello,  " + name + "! Happy " + day + "!")
+
+main()
+
