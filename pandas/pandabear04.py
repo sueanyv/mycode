a50e8330223231eb777e36544c335e3924c2b992
sueanyv <valenciasue@hotmail.com>
2023-01-11 16:07:50 +0000

    classwork

---
+++ pandas/pandabear04.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+def main():
+    # create a dataframe ciscocsv
+    ciscocsv = pd.read_csv("ciscodata.csv")
+    # create a dataframe ciscojson
+    ciscojson = pd.read_json("ciscodata2.json")
+
+    # The line below concats and reapplies the index value
+    ciscodf = pd.concat([ciscocsv, ciscojson], ignore_index=True, sort=False)
+
+    ## export to json
+    ## do not include index number
+    ciscodf.to_json("combined_ciscodata.json", orient="records")
+
+    ## export to csv
+    ## do not include index number
+    ciscodf.to_csv("combined_ciscodata.csv", index=False)
+
+    ## export to Excel
+    ## do not include index number to xls
+    ciscodf.to_excel("combined_ciscodata.xls", index=False)
+    ## do not include index number to xlsx
+    ciscodf.to_excel("combined_ciscodata.xlsx", index=False)
+
+    ## create a python dictionary
+    ## do not include index number
+    x = ciscodf.to_dict(orient='records')
+    print(x)
+
+if __name__ == "__main__":
+    main()
+def main():
+    # create a dataframe ciscocsv
+    ciscocsv = pd.read_csv("ciscodata.csv")
+    # create a dataframe ciscojson
+    ciscojson = pd.read_json("ciscodata2.json")
+
+    # The line below concats and reapplies the index value
+    ciscodf = pd.concat([ciscocsv, ciscojson], ignore_index=True, sort=False)
+
+    ## print to the screen the re-indexed dataframe
+    print(ciscodf)
+    
+    ## print a blankline
+    print()
+
+    ## export to json
+    ciscodf.to_json("combined_ciscodata.json")
+
+    ## export to csv
+    ciscodf.to_csv("combined_ciscodata.csv")
+    
+    ## export to Excel
+    ciscodf.to_excel("combined_ciscodata.xls")
+    
+    ## create a python dictionary
+    x = ciscodf.to_dict()
+    print(x)
+    
+if __name__ == "__main__":
+    main()
+
